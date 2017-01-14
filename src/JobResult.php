5c647272670b674011910907e9639261e6fdd3c0
Ben Roberts <beroberts@wayfair.com>
2017-01-14 13:18:45 -0500

    add some helper value objects

---
+++ src/JobResult.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: beroberts
+ * Date: 1/14/17
+ * Time: 1:04 PM
+ */
+
+namespace WF\Hypernova;
+
+
+class JobResult
+{
+    /**
+     * @var string
+     */
+    public $error;
+
+    /**
+     * @var string rendered HTML
+     */
+    public $html;
+}
---
+++ src/Response.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: beroberts
+ * Date: 1/14/17
+ * Time: 12:58 PM
+ */
+
+namespace WF\Hypernova;
+
+
+class Response
+{
+    /**
+     * @var string
+     */
+    public $error;
+
+    /**
+     * @var \WF\Hypernova\JobResult[]
+     */
+    public $results;
+}
